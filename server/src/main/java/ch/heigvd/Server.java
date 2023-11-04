7df6548f9ac7b14b6a3f7cb83e65a66be83aa028
theodrosrun <theodros.mulugeta@runciter.ch>
2023-11-04 22:48:45 +0100

    Server implementation

--- server/src/main/java/ch/heigvd/Server.java
+++ server/src/main/java/ch/heigvd/Server.java
@@ -1,11 +1,30 @@
 package ch.heigvd;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Server {
-    public static void main(String[] args) {
+    private final static Logger LOG = Logger.getLogger(Server.class.getName());
 
+    public static void main(String[] args) {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
+        (new Server()).start();
     }
 
+    private void start() {
+        int port = 20000;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                LOG.log(Level.INFO, "Waiting for a new client on port {0}", port);
+                Socket clientSocket = serverSocket.accept();
+                LOG.info("A new client has arrived. Starting a new thread and delegating work to a new servant...");
+                new Thread(new ServerWorker(clientSocket)).start();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- server/src/main/java/ch/heigvd/ServerWorker.java
+++ server/src/main/java/ch/heigvd/ServerWorker.java
@@ -1,11 +1,39 @@
 package ch.heigvd;
 
-public class ServerWorker {
-    public void Sender() {
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class ServerWorker implements Runnable {
+    private final static Logger LOG = Logger.getLogger(ServerWorker.class.getName());
+    Socket clientSocket;
+    BufferedReader clientInput = null;
+    PrintWriter serverOutput = null;
+
+    public ServerWorker(Socket clientSocket) {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
+        this.clientSocket = clientSocket;
+        try {
+            clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            serverOutput = new PrintWriter(clientSocket.getOutputStream());
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, ex.getMessage(), ex);
+        }
     }
 
-    public void Receiver() {
+    @Override
+    public void run() {
+        try {
+            while ((clientInput.readLine()) != null) {
+                serverOutput.write("Hello client!");
+                serverOutput.flush();
+            }
 
+            // clientInput.close();
+            // serverOutput.close();
+        } catch (IOException ex) {
+
+        }
     }
 }
