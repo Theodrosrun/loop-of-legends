645d7a3db2c5410f95cbea0a50a45e3ae90dc1ac
theodrosrun <theodros.mulugeta@runciter.ch>
2023-11-07 16:01:26 +0100

    Implementing enum message

---
+++ lib/src/main/java/ch/heigvd/Message.java
@@ -0,0 +1,37 @@
+package ch.heigvd;
+
+public enum Message {
+    INIT("INIT"),
+    DONE("DONE"),
+    LOBB("LOBB"),
+    JOIN("JOIN"),
+    RADY("RADY"),
+    STRT("STRT"),
+    DIRE("DIRE"),
+    UPTE("UPTE"),
+    ENDD("ENDD");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static Message fromString(String message) {
+        for (Message m : Message.values()) {
+            if (m.getMessage().equalsIgnoreCase(message)) {
+                return m;
+            }
+        }
+        throw new IllegalArgumentException("No constant with text " + message + " found");
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
