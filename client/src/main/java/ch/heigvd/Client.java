5981a96120c4559d24648d7a71b0c5653904bffb
Theodros <theodros.mulugeta@runciter.ch>
2023-11-24 19:33:22 +0100

    Removing space

--- client/src/main/java/ch/heigvd/Client.java
+++ client/src/main/java/ch/heigvd/Client.java
@@ -268,8 +268,6 @@ public class Client {
         exit(0);
     }
 
-
-
     public static void main(String[] args) {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
