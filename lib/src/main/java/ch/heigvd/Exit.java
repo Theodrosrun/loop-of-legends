531674fffaa9eab26b19dc1d4b9580dd98327a9e
AlexandreIorio <alexandre.iorio@heig-vd.ch>
2023-11-26 19:56:45 +0100

    REFACTOR: Lib

--- lib/src/main/java/ch/heigvd/Exit.java
+++ lib/src/main/java/ch/heigvd/Exit.java
@@ -8,6 +8,9 @@ import java.util.logging.Level;
 
 import static java.lang.System.exit;
 
+/**
+ * This class is used to close the connection properly
+ */
 public class Exit implements Runnable {
 
     /**
@@ -24,12 +27,21 @@ public class Exit implements Runnable {
      */
     private BufferedReader serverInput;
 
+    /**
+     * The constructor of the class
+     * @param socket the socket used to communicate with the server/client
+     * @param serverOutput the output stream used to communicate with the server/client
+     * @param serverInput the input stream used to communicate with the server/client
+     */
     public Exit(Socket socket, BufferedWriter serverOutput, BufferedReader serverInput) {
         this.serverOutput = serverOutput;
         this.serverInput = serverInput;
         this.socket = socket;
     }
 
+    /**
+     * The run method of the thread
+     */
     @Override
     public void run() {
         if (socket != null) {
@@ -37,6 +49,9 @@ public class Exit implements Runnable {
         }
     }
 
+    /**
+     * This method is used to close the connection properly
+     */
     private void closeServer() {
         try {
 
--- lib/src/main/java/ch/heigvd/Message.java
+++ lib/src/main/java/ch/heigvd/Message.java
@@ -3,34 +3,51 @@ package ch.heigvd;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+/**
+ * Enumeration of the different messages that can be sent by the server
+ * or the client.
+ */
 public enum Message {
     INIT("INIT"),
     DONE("DONE"),
     LOBB("LOBB"),
     JOIN("JOIN"),
     RADY("RADY"),
-    STRT("STRT"),
-    MSGG("MSGG"),
     REPT("REPT"),
     EROR("EROR"),
     DIRE("DIRE"),
     UPTE("UPTE"),
-    ENDD("ENDD"),
     QUIT("QUIT"),
     UNKN("UNKN");
+
+    /**
+     * The separator used to separate the message from the data
+     */
     final static String SEPARATOR = " ";
+
+    /**
+     * The end of transmission character
+     */
     final static char EOT = 0x04;
+
+    /**
+     * The message
+     */
     private final String message;
 
+    /**
+     * Constructor
+     * @param message the message
+     */
     Message(String message) {
         this.message = message;
     }
 
-    @Override
-    public String toString() {
-        return message;
-    }
-
+    /**
+     * Get the message from a string
+     * @param message the string
+     * @return the message
+     */
     public static Message fromString(String message) {
         for (Message m : Message.values()) {
             if (m.toString().equalsIgnoreCase(message)) {
@@ -40,6 +57,12 @@ public enum Message {
         return UNKN;
     }
 
+    /**
+     * Set the command to send to the server
+     * @param message the message
+     * @param data the data
+     * @return the command
+     */
     public static String setCommand(Message message, String data){
         if (data == null) {
             return message.toString() + EOT;
@@ -48,10 +71,21 @@ public enum Message {
         }
     }
 
+    /**
+     * Set the command to send to the server
+     * @param message the message
+     * @return the command
+     */
     public static String setCommand(Message message) {
         return setCommand(message, null);
     }
 
+    /**
+     * Get the response from the server
+     * @param reader the reader
+     * @return the response
+     * @throws IOException if an I/O error occurs
+     */
     public static String getResponse(BufferedReader reader) throws IOException {
         StringBuilder response = new StringBuilder();
         int c;
@@ -66,12 +100,27 @@ public enum Message {
         return response.toString();
     }
 
+    /**
+     * Get the message from a string
+     * @param string the string
+     * @return the message
+     */
     public static String getMessage(String string) {
         return string.split(SEPARATOR, 2)[0];
     }
 
+    /**
+     * Get the data from a string
+     * @param string the string
+     * @return the data
+     */
     public static String getData(String string) {
         String[] tab = string.split(SEPARATOR, 2);
         return tab.length > 1 ? tab[1] : "";
+    }
+
+    @Override
+    public String toString() {
+        return message;
     }
 }
